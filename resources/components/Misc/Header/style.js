7243980de43102d44a467b35aaef9805ee79a5db
KDani-99 <porsekin@gmail.com>
2020-10-11 21:22:49 +0200

    fix(ios): style issues (platform specific)

--- resources/components/Misc/Header/style.js
+++ resources/components/Misc/Header/style.js
@@ -1,10 +1,11 @@
-import {StyleSheet} from 'react-native';
+import {Platform,StyleSheet} from 'react-native';
 
 const style = StyleSheet.create(
     {
         header:{
             color:'rgb(56, 62, 83)',
-            fontSize:22
+            fontSize:22,
+            marginTop:Platform.OS === 'ios' ? 30 : 0
         },
         description:{
             fontSize:16,
